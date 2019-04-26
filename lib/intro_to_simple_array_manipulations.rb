ae6fc4ea96d3e5476ed8ba22153ca23a104fa0c1
Renée Cruz <reneebasscruz@gmail.com>
2019-04-26 22:36:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -29,5 +29,5 @@ def using_concat(my_favorite_things, all_my_favs)
 end
 
 def using_insert(list_of_esoteric_programming_languages, another_esoteric_language)
-  list_of_esoteric_programming_languages.insert([4] = 'another_esoteric_language')
+  list_of_esoteric_programming_languages.insert('Malbolge')
 end
