6e9e7a684b8a6a2c33c4f600a0f086bb9ee0adef
Renée Cruz <reneebasscruz@gmail.com>
2019-04-26 22:57:50 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,49 @@
+
+
+def using_push(countries_in_west_africa, next_country)
+  countries_in_west_africa.push('Niger')
+end
+
+def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
+  neighborhoods_in_northwest_brooklyn.unshift('Brooklyn Heights')
+end
+
+def using_pop(great_hits_of_the_nineties)
+  great_hits_of_the_nineties.pop
+end
+
+def pop_with_args(chars_in_game_of_thrones)
+  chars_in_game_of_thrones.pop(2)
+end
+
+def using_shift(my_favorite_cities)
+  my_favorite_cities.shift
+end
+
+def shift_with_args(icecream_brands)
+  icecream_brands.shift(2)
+end
+
+def using_concat(my_favorite_things, all_my_favs)
+  all_my_favs = my_favorite_things.concat(all_my_favs)
+end
+
+def using_insert(list_of_esoteric_programming_languages, another_esoteric_language)
+  list_of_esoteric_programming_languages.insert(4,'Malbolge')
+end
+
+def using_uniq(captain_planet_and_the_planeteers)
+  captain_planet_and_the_planeteers.uniq
+end
+
+def using_flatten(private_colleges_in_newyork)
+  private_colleges_in_newyork.flatten
+end
+
+def using_delete(instructors,no_offense_steven)
+  instructors.delete('Steven')
+end
+
+def using_delete_at(famous_robots, deleted_robot)
+  famous_robots.delete_at(2)
+end
