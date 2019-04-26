474a9080b6aa5e1704b5867db2f179f622da42be
Renée Cruz <reneebasscruz@gmail.com>
2019-04-26 21:58:54 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,11 @@
+def using_push(countries_in_west_africa, next_country)
+  countries_in_west_africa.push('Niger')
+end
+
+def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
+  neighborhoods_in_northwest_brooklyn.unshift('Brooklyn Heights')
+end
+
+def using_pop()
+  
+end
