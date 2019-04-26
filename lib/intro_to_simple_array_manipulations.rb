41bd20407657837b74c1f8c64f950ec3fcc1cbe6
Renée Cruz <reneebasscruz@gmail.com>
2019-04-26 22:13:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -7,12 +7,12 @@ end
 def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
   neighborhoods_in_northwest_brooklyn.unshift('Brooklyn Heights')
 end
-require pry
+
 def using_pop(great_hits_of_the_nineties)
   great_hits_of_the_nineties.pop('Losing My Religion')
   return using_pop
 end
-binding.pry
+
 def pop_with_args(chars_in_game_of_thrones)
   chars_in_game_of_thrones.pop(2)
 end
