e058f56f3d264190c2639e96d9499f6c096f5bf7
Renée Cruz <reneebasscruz@gmail.com>
2019-04-26 22:19:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,7 +15,8 @@ end
 def pop_with_args(chars_in_game_of_thrones)
   chars_in_game_of_thrones.pop(2)
 end
-
+require pry
 def using_shift(my_favorite_cities)
   my_favorite_cities.shift
 end
+binding.pry
