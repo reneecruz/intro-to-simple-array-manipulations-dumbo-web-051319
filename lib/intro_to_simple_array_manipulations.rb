c629b1a67b580d3305ea277778faf4f1b44024f2
Renée Cruz <reneebasscruz@gmail.com>
2019-04-26 22:40:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -32,5 +32,6 @@ def using_insert(list_of_esoteric_programming_languages, another_esoteric_langua
   list_of_esoteric_programming_languages.insert(4,'Malbolge')
 end
 
-def using_uniq()
+def using_uniq(captain_planet_and_the_planeteers)
+  captain_planet_and_the_planeteers.uniq
 end
