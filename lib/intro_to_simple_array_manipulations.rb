78d7d56d16281de5dd617dd2fdf035e919fdc1d5
Renée Cruz <reneebasscruz@gmail.com>
2019-04-26 22:30:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -25,5 +25,5 @@ def shift_with_args(icecream_brands)
 end
 
 def using_concat(my_favorite_things, all_my_favs)
-  all_my_favs.concat(my_favorite_things)
+  all_my_favs = my_favorite_things.concat(all_my_favs)
 end
